c65d71c1d78d55e53492570beb25e943e79ea8eb
mundu-wa-nyumba <homes.patch-0n@icloud.com>
2024-05-23 13:37:57 +0100

    first commit

---
+++ src/test/java/com/mwn/Greenhouse/GreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwn.Greenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
